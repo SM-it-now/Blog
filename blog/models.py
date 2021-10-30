b4e58a3a040ed6c1e36f65c67c5a508258c7e25f
iseungmin <ibb09@naver.com>
2021-10-30 23:36:28 +0900

    21.10.30 (첨부파일이 있을 경우, 포스트 상세 페이지에 다운로드 버튼 추가(확장자명 마다 다른 다운로드 이미지 출력))

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 
@@ -9,8 +10,21 @@ class Post(models.Model):
 
     head_img = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    # get_absolute_url을 상세페이지 이동 url로 사용하기 위한 함수 구현.
+    def get_absolute_url(self):
+        return f'blog/{self.pk}/'
+
+    # 상세페이지의 file 확장자 명을 출력하기 위한 함수
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    # 상세페이지의 file 확장자를 찾아내는 함수.
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]

421893d34187425d1a2d28400be119d1e2bfe271
iseungmin <ibb09@naver.com>
2021-11-15 16:08:21 +0900

    21.11.15 (comment 모델 생성과 post 상세 페이지의 댓글 출력.)

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 # Register your models here.
 
@@ -17,5 +17,6 @@ class TagAdmin(admin.ModelAdmin):
 
 # admin 페이지에 출력.
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -66,4 +66,16 @@ class Post(models.Model):
         return self.get_file_name().split('.')[-1]
 
 
+# 댓글 기능 모델
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} :: {}'.format(self.author, self.content)
+
+
 
